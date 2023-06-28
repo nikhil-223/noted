c64c48bac1e75fa35d1bc633078cbe540ee44e9a
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-06-28 12:53:12 +0530

    Update Notes.jsx

--- components/Notes.jsx
+++ components/Notes.jsx
@@ -10,15 +10,18 @@ const Notes = () => {
 	const { data: session } = useSession();
 
 	const [notes, setNotes] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		const fetchNotes = async () => {
+			setIsLoading(true);
 			const response = await fetch(`/api/user/${session?.user.id}/notes`);
 			const data = await response.json();
       
 			if (data) {
         data.reverse();
 				setNotes(data);
+				setIsLoading(false);
 			}
 		};
 
@@ -27,20 +30,30 @@ const Notes = () => {
 		}
 	},[session?.user.id]);
 
-	return (<section
+	return (
+		<section
 			id="notes"
 			className={`sm:w-full flex sm:justify-center md:justify-start flex-wrap px-2 sm:py-5 md:pt-0 gap-2 ${
 				pathname === "/notes" && "mt-8"
 			}`}>
-			{notes.length===0? <div className="w-full text-2xl font-bold flex justify-center">You don't have any notes</div>:notes.map((note) => (
-				<NoteItem
-					key={note._id}
-					id={note._id}
-					title={note.title}
-					note={note.note}
-				/>
-			))}
-		</section>)
+			{isLoading ? (
+				<div className="w-full flex justify-center"> loading...</div>
+			) : notes.length === 0 ? (
+				<div className="w-full text-2xl font-bold flex justify-center">
+					You don't have any notes
+				</div>
+			) : (
+				notes.map((note) => (
+					<NoteItem
+						key={note._id}
+						id={note._id}
+						title={note.title}
+						note={note.note}
+					/>
+				))
+			)}
+		</section>
+	);
 };
 
 export default Notes;
